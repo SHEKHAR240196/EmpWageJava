3ca8291d9653d4fd5048e0f016c80882c4794869
shekhar <shekharpatle999@gmail.com>
2021-04-05 14:39:07 +0530

    added: PartTimeEmpwage

--- employee.sh
+++ employee.sh
@@ -1,27 +1,21 @@
 package Emp;
 import java.util.Random;
-
-public class DailyEmpWage {
-	public static void main(String[] args) 
-	{
-
-	int a = 1;
-	int EmpWagePerHrs = 20;
-	int EmpHrs = 8;
-
-	Random rand = new Random();
-
-	int Empcheck = rand.nextInt(2);
-
-	if (Empcheck == a)
-
-	System.out.println("Employee Daily Wage is :" +Math.multiplyExact(EmpHrs,EmpWagePerHrs));
-
-	else
-		System.out.println("Employee Daily Wage is 0");
-
-
-
-
-}
+public class PartTimeEmpWage {
+	
+	public static void main(String[] args) {
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int empRatePerHr = 20;
+		double empCheck = Math.floor(Math.random()*10)%3;
+		
+		System.out.println(empCheck);
+		int empHrs= 8;
+		if( IS_FULL_TIME == empCheck)
+			empHrs=8;
+		else if(IS_PART_TIME == empCheck)
+			empHrs=4;
+		else
+			empHrs=0;
+	System.out.println("Employee  Wage is:" + Math.multiplyExact(empRatePerHr, empHrs));
+	}
 }
